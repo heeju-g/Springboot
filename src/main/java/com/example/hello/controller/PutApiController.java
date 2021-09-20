6f9aa87d898f4b342394c7484d37ae30b95cb10d
Heeju Sim <heeju01226@gmail.com>
2021-09-20 14:44:34 +0900

    path variable

--- src/main/java/com/example/hello/controller/PutApiController.java
+++ src/main/java/com/example/hello/controller/PutApiController.java
@@ -2,17 +2,14 @@ package com.example.hello.controller;
 
 
 import com.example.hello.dto.PutRequestDto;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class PutApiController {
-    @PutMapping("/put")
-    public PutRequestDto put(@RequestBody PutRequestDto requestDto){
-        System.out.println(requestDto);
+    @PutMapping("/put/{userId}")
+    public PutRequestDto put(@RequestBody PutRequestDto requestDto, @PathVariable long userId){
+        System.out.println(userId);
         return requestDto;
     }
 }
